202ca078fa9854ce6f9d24c0c5a2275e63890e74
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-10-14 11:05:43 -0700

    .gitignore

--- models.py
+++ models.py
@@ -1,1 +1,13 @@
 """Models for Blogly."""
+from sqlalchemy import 
+
+class User(db.Model):
+    """ User Model """
+
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    first_name = db.Column()
