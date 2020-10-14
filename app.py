1fdc478f762074cd2eeed339737e1d6bccfd7eb7
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-10-14 11:46:29 -0700

    created app skeleton and html pages

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,3 +11,33 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+toolbar = DebugToolbarExtension
+
+
+@app.route("/")
+def start():
+    users = User.query.all()
+    return render_template("user_listing.html", users=users)
+
+
+@app.route("/users")
+
+@app.route("/users/new")
+
+@app.route("/users/new")
+# POST
+
+@app.route("/users/<int:id>")
+
+@app.route("/users/<int:id>/edit")
+
+@app.route("/users/<int:id>/edit")
+# POST
+
+@app.route("/users/<int:id>/delete")
+# DELETE
+
+
+
+
--- models.py
+++ models.py
@@ -1,5 +1,9 @@
 """Models for Blogly."""
-from sqlalchemy import 
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 
 class User(db.Model):
     """ User Model """
@@ -9,5 +13,14 @@ class User(db.Model):
     id = db.Column(db.Integer,
                    primary_key=True,
                    autoincrement=True)
+    first_name = db.Column(db.String(50),
+                           nullable=False)
+    last_name = db.Column(db.String(50),
+                          nullable=False)
+    image_url = db.Column(db.Text,
+                          nullable=True,
+                          default='https://immedilet-invest.com/wp-content/uploads/2016/01/user-placeholder.jpg')
 
-    first_name = db.Column()
+    def __repr__(self):
+        s = self
+        return f"User {s.id} {s.first_name} {s.last_name} {s.image_url}"
