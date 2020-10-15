7eae629cda92d649a5fecad29b8eeda499c77378
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-10-15 12:08:53 -0700

    added posts to user detail, added new post functionality, cleaned up user detail look

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, render_template, redirect, request
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -21,9 +21,9 @@ def start():
     return redirect("/users")
 
 
-@app.route("/users")
+@app.route("/users", strict_slashes=False)
 def show_users():
-    users = User.query.all()
+    users = User.query.order_by(User.last_name, User.first_name).all()
     return render_template("user_listing.html", users=users)
 
 
@@ -31,13 +31,14 @@ def show_users():
 def show_new_user_form():
     return render_template("user_new.html")
 
+
 @app.route("/users/new", methods=["POST"])
 def process_new_user():
 
     first_name = request.form['first-name']
     last_name = request.form['last-name']
     image_url = request.form['image-url']
-    image_url = image_url if image_url else "https://immedilet-invest.com/wp-content/uploads/2016/01/user-placeholder.jpg"
+    image_url = image_url if image_url else "https://bit.ly/3lUJbon"
 
     User.create_user(first_name, last_name, image_url)
 
@@ -66,11 +67,12 @@ def process_edit_user(user_id):
     first_name = request.form["first-name"]
     last_name = request.form["last-name"]
     image_url = request.form["image-url"]
-    image_url = image_url if image_url else "https://immedilet-invest.com/wp-content/uploads/2016/01/user-placeholder.jpg"
-    
+    image_url = image_url if image_url else "https://bit.ly/3lUJbon"
+
     user.update_user(first_name, last_name, image_url)
 
     return redirect("/users")
+
 
 @app.route("/users/<int:user_id>/delete", methods=["POST"])
 def process_delete_user(user_id):
@@ -79,3 +81,27 @@ def process_delete_user(user_id):
     user.delete_user(user)
 
     return redirect("/users")
+
+
+@app.route("/users/<int:user_id>/posts/new")
+def show_new_post_form(user_id):
+    user = User.query.get_or_404(user_id)
+
+    return render_template("post_new.html", user=user)
+
+
+@app.route("/users/<int:user_id>/posts/new", methods=["POST"])
+def process_post_form(user_id):
+
+    title = request.form['title']
+    content = request.form['content']
+
+    Post.create_post(title, content, user_id)
+
+    return redirect(f"/users/{user_id}")
+
+
+# @app.route("/posts/<int:post_id>")
+# @app.route("/posts/<int:post_id>/edit")
+# @app.route("/posts/<int:post_id>/edit", methods=["POST"])
+# @app.route("/posts/<int:post_id>/delete", methods=["POST"])
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -13,16 +14,27 @@ class User(db.Model):
     id = db.Column(db.Integer,
                    primary_key=True,
                    autoincrement=True)
+
     first_name = db.Column(db.String(50),
                            nullable=False)
+
     last_name = db.Column(db.String(50),
                           nullable=False)
+
     image_url = db.Column(db.Text,
                           nullable=True)
 
+    posts = db.relationship('Post')
+
     def __repr__(self):
         s = self
-        return f"User {s.id} {s.first_name} {s.last_name} {s.image_url}"
+        return f"<User {s.id} {s.first_name} {s.last_name} {s.image_url}>"
+
+    @property
+    def full_name(self):
+        """Return full name of user."""
+
+        return f"{self.first_name} {self.last_name}"
 
     @staticmethod
     def create_user(first_name, last_name, image_url):
@@ -36,9 +48,41 @@ class User(db.Model):
         self.image_url = image_url
         db.session.commit()
 
-    
     def delete_user(self, user):
         db.session.delete(user)
+        db.session.commit()
+
+
+class Post(db.Model):
+    """ Blog post model"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    title = db.Column(db.String(50),
+                      nullable=False)
+
+    content = db.Column(db.Text)
+
+    created_at = db.Column(db.DateTime,
+                           default=datetime.datetime.utcnow())
+
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey("users.id"))
+
+    user = db.relationship('User')
+
+    def __repr__(self):
+        s = self
+        return f"<Post {s.id} {s.title} {s.content} {s.created_at} {s.user_id}>"
+
+    @staticmethod
+    def create_post(title, content, user_id):
+        new_post = Post(title=title, content=content, user_id=user_id)
+        db.session.add(new_post)
         db.session.commit()
 
 
@@ -51,12 +95,20 @@ def example_data():
     # Add users
     u1 = User(first_name='Mack',
               last_name='Cooper',
-              image_url='https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+              image_url='https://bit.ly/3462Qfn')
     u2 = User(first_name='Andrew',
               last_name='Dietrich',
-              image_url='https://www.postplanner.com/hs-fs/hub/513577/file-2886416984-png/blog-files/facebook-profile-pic-vs-cover-photo-sq.png?width=250&height=250&name=facebook-profile-pic-vs-cover-photo-sq.png')
+              image_url='https://bit.ly/3k4epcb')
 
-    db.session.add_all([u1, u2])
+    p1 = Post(title='My First Post',
+              content='This is great. Yes it is.',
+              user_id=1)
+
+    p2 = Post(title='Things I learned about SQAlchemy through the fire',
+              content='It began on a caffeinated Wednesday morning...',
+              user_id=2)
+
+    db.session.add_all([u1, u2, p1, p2])
     db.session.commit()
 
 
@@ -65,6 +117,7 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
 
 if __name__ == "__main__":
     # As a convenience, if we run this module interactively, it will leave
